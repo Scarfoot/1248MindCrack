8e170476e65b7d4699c4e68bc861ebc060c3f560
Jimmy Allen <jimmy1248@gmail.com>
2013-06-24 18:20:50 +1200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+  <img src="banner.png">
+	<div class="navbar">
+	  <div class="navbar-inner">
+	    <a class="brand" href="#">Title</a>
+	    <ul class="nav">
+	      <li class="active"><a href="#">Home</a></li>
+	      <li><a href="tutorials.php">Tutorials</a></li>
+	      <li><a href="#">Link</a></li>
+	    </ul>
+	  </div>
+	</div>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
